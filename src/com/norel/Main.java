f3dcab2fe0373b5909f30390d40c5dd3ef285f0a
PreciseJR <jrmiller525@yahoo.com>
2023-04-26 22:41:42 -0700

    Basics for classes and definitions

--- src/com/norel/Main.java
+++ src/com/norel/Main.java
@@ -3,5 +3,37 @@ package com.norel;
 public class Main {
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("hello");
     }
+
+    /*
+    Class: like a blueprint/template to create objects
+
+    Object: instance of a class, has behavior and state
+
+    Method: Behavior of an object
+
+    Public Classes: Can be accessed from anywhere in the program
+        Why? Can be accessed by other developers to use if library or API, also be used as an entry point
+
+    Private Classes: Class: Can only be accessed within the same class its declared
+        Why? in general make classes as private as possible to improve security and less complex.
+             If you have a helper class or supporting class is used within larger/related classes.
+             Can help coupling and improve encapsulation
+
+    Void Classes: Indicates the method doesn't return any value. Performs action but no output/result
+     */
+
+
+    /*
+    ~ Identifiers ~
+        Names of local variables/instances/class variables/labels but also for classes/packages/modules/methods
+        Case sensitive, Cannot be keywords
+
+    ~ Modifiers ~
+        Control the scope of classes and methods
+        Access: default, private, public, protected
+        Non-access: final, abstract, static, transient, synchronized, volatile, native
+
+     */
 }
